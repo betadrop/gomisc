a6a5e6f12602ba09b744051e10026d45cb6ae43c
betadrop <betadrop@@users.noreply.github.com>
2015-01-18 13:30:35 +0100

    xml2json progress

--- xml2json/xml2json.go
+++ xml2json/xml2json.go
@@ -7,6 +7,46 @@ import (
 	"strings"
 )
 
+const (
+	INDENT = "    "
+)
+
+func isStart(token xml.Token) bool {
+	_, ok := token.(xml.StartElement)
+	return ok
+}
+
+func startName(token xml.Token) string {
+	if start, ok := token.(xml.StartElement); ok {
+		return start.Name.Local
+	}
+	return ""
+}
+
+func isEnd(token xml.Token) bool {
+	_, ok := token.(xml.EndElement)
+	return ok
+}
+
+func endName(token xml.Token) string {
+	if end, ok := token.(xml.EndElement); ok {
+		return end.Name.Local
+	}
+	return ""
+}
+
+func isChar(token xml.Token) bool {
+	_, ok := token.(xml.CharData)
+	return ok
+}
+
+func getCharData(token xml.Token) string {
+	if charData, ok := token.(xml.CharData); ok {
+		return string(charData)
+	}
+	return ""
+}
+
 func main() {
 	args := os.Args
 	if len(args) < 2 {
@@ -19,16 +59,12 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading "+filename+":", err)
 	}
 	decoder := xml.NewDecoder(file)
-	var stack[] string;
-        type TokenType int
-	const ( 
-	   NONE TokenType = iota
-	   START
-	   CHARDATA
-	   END
-        )
-	var lastToken TokenType = NONE
+	var stack []string
+	var lastElement string
+	var lastToken xml.Token
+	first := true
 	for {
+		var token xml.Token
 		token, err := decoder.Token()
 		if token == nil {
 			break
@@ -37,32 +73,51 @@ func main() {
 			fmt.Fprintln(os.Stderr, "token: ", err)
 			break
 		}
-		if start, ok := token.(xml.StartElement); ok {
-			if lastToken == START {
-			   for i := 0; i < len(stack); i++ {
-			       fmt.Print("   ");
-			   }
-			   fmt.Println(stack[len(stack) - 1]);
+		// Skip empty character data
+		if isChar(token) {
+			str := string(getCharData(token))
+			if len(strings.Trim(str, " \n\t")) == 0 {
+				continue
 			}
-                        stack = append(stack, start.Name.Local)
-			lastToken = START
-		} 
-		if _, ok := token.(xml.EndElement); ok {
-		   	stack = stack[:len(stack)-1]
-			lastToken = END
 		}
-		if charData, ok := token.(xml.CharData); ok {
-		        str := string(charData)
-                        trimmed := strings.Trim(str, " \n\t")
-                        if len(trimmed) > 0 {
-                                if len(stack) > 0 {
-				   for i := 0; i < len(stack); i++ {
-				       fmt.Print("   ");
-				   }
-			  	   fmt.Println(stack[len(stack) - 1] + ": " + str);
+		if isStart(token) {
+			if isStart(lastToken) {
+				if first {
+					first = false
+				} else {
+					fmt.Println()
 				}
-				lastToken = CHARDATA
+				for i := 0; i < len(stack)-1; i++ {
+					fmt.Print(INDENT)
+				}
+				fmt.Print(stack[len(stack)-1])
+			}
+			stack = append(stack, startName(token))
+		}
+		if isEnd(token) {
+			lastElement = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		if isChar(token) {
+			str := strings.Trim(getCharData(token), " \n\t")
+			if len(stack) > 0 {
+				// If the element is the same a last time then
+				// display it like an array.
+				currentElement := stack[len(stack)-1]
+				if currentElement == lastElement {
+					fmt.Print(", " + str)
+				} else {
+					fmt.Println()
+					for i := 0; i < len(stack)-1; i++ {
+						fmt.Print(INDENT)
+					}
+					fmt.Print(stack[len(stack)-1] + ": " + str)
+				}
 			}
 		}
+		lastToken = token
+	}
+	if !first {
+		fmt.Println()
 	}
 }
